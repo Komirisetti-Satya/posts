fbc5f3f0f884ac7e96bc40c2c84d5ce764ab0b28
Komirisetti-Satya <83025754+Komirisetti-Satya@users.noreply.github.com>
2021-08-11 22:10:37 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import Createpost from './components/createpost';
+import './App.css';
+
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        
+        <Createpost/>
+      </div>
+    );
+  }
+}
+
+export default App;
